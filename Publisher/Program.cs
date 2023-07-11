57893a5edb29f18ecfe2e063657760b47adc2b52
Kate B <kate@ekbys.com>
2023-07-11 11:46:08 +0530

    Example implemented

---
+++ Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using FastEndpoints;
+
+var bld = WebApplication.CreateBuilder();
+bld.WebHost.ConfigureKestrel(o => o.ListenLocalhost(5000));
+var app = bld.Build();
+
+app.MapRemote("http://localhost:6000", c =>
+{
+    c.RegisterEvent<SomethingHappened>();
+});
+
+app.MapGet("/event/{name}", async (string name) =>
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        await new SomethingHappened
+        {
+            Id = i,
+            Description = name
+        }
+        .RemotePublishAsync();
+
+        await Task.Delay(500);
+    }
+    return Results.Ok("events published!");
+});
+
+app.Run();
---
+++ Subscriber/Program.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+using FastEndpoints;
+using Subscriber;
+
+var bld = WebApplication.CreateBuilder();
+bld.WebHost.ConfigureKestrel(o => o.ListenLocalhost(7000));
+var app = bld.Build();
+
+app.MapRemote("http://localhost:6000", c =>
+{
+    c.Subscribe<SomethingHappened, WhenSomethingHappens>();
+});
+
+app.Run();
